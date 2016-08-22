3f704acc9113063483c4cb36d3a6fac9fe916216
T6A User <t6a@mobiles-imac-28.local>
2016-08-22 14:18:21 -0600

    Finished Lab 4.

--- LABs/Lab04_Optionals.playground/Contents.swift
+++ LABs/Lab04_Optionals.playground/Contents.swift
@@ -6,6 +6,12 @@ import UIKit
 
 var myNum = {$0*15/(sqrt($1)-10.5)}(10,5)
 
+func myNumFunction(num1 num1:Float, num2:Float) -> Float{
+    return num1*15/(sqrt(num2)-10.5)
+}
+let result: Float
+result = myNumFunction(num1: 10, num2: 5)
+
 // The fuction below takes an array and returns an optional "tuple"
 // (yes you should know what a tuple is ... it was in the reading).  
 // This tuple is an optional and will either be nil if the array cannot 
@@ -32,6 +38,7 @@ print("Example: \(exampleArray.description)")
 // and print it (not as an optional) Note: a ".0" appended to the tuple  
 // name  will access the first component of the tuple (e.g. arrayTuple1.0),
 // see exampleArray above for use of the .description property
+print(arrayTuple1!.0.description)
 
 
 // Task C - print arrayTuple2's second component or an error message if
@@ -39,6 +46,16 @@ print("Example: \(exampleArray.description)")
 // take more than a single line of code to do correctly.  Note: a ".1" will
 // access the second component (e.g. arrayTuple2.1).  Test your code with
 // an array that can be split and then one that cannont.
+if let temp = arrayTuple2?.1{
+    print(temp)
+} else {
+    print("ERROR: Second component is nil.")
+}
+if let temp = arrayTuple1?.1{
+    print(temp)
+} else {
+    print("ERROR: Second component is nil.")
+}
 
 
 // If you finish you should start reading chapter 4 or for fun you can try 
