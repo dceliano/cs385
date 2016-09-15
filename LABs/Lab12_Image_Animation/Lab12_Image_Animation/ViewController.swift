3393e34234c7773e4b6736375348f7b98aa41748
T6A User <t6a@mobiles-imac-28.local>
2016-09-15 14:13:09 -0600

    Finished Lab 12 (got the bird going).

---
+++ LABs/Lab12_Image_Animation/Lab12_Image_Animation/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Lab12_Image_Animation
+//
+//  Created by Troy Weingart on 9/14/16.
+//  Copyright © 2016 Troy Weingart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var falconImageList: [UIImage] = []
+    
+    @IBOutlet weak var falcon1: UIImageView!
+    @IBOutlet weak var falcon2: UIImageView!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var flyButton: UIButton!
+    @IBOutlet weak var speedStepper: UIStepper!
+    @IBOutlet weak var speedSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for i in 1...21{
+            falconImageList += [UIImage(named:"falcon-\(i).png")!]
+        }
+        falcon1.animationImages = falconImageList
+        falcon1.animationDuration = Double(2.0 - speedSlider.value)
+    }
+    @IBAction func flyLand(sender: UIButton) {
+        if(!falcon1.isAnimating()){
+            falcon1.startAnimating()
+            falcon2.image = UIImage.animatedImageWithImages(falconImageList, duration: Double(2.0-speedSlider.value))
+            speedLabel.text = "Speed of an unlaiden Falcon: \(1.0/(1.01-speedSlider.value))"
+            flyButton.setTitle("Land", forState: .Normal)
+        }
+        else{
+            falcon1.stopAnimating()
+            falcon2.image = UIImage(named:"falcon-1.png")
+            speedLabel.text = "Speed of an unlaiden Falcon: 0.00"
+            flyButton.setTitle("Fly Away", forState: .Normal)
+        }
+    }
+    @IBAction func sliderChange(sender: AnyObject) {
+        speedStepper.value = Double(speedSlider.value)
+        changeDuration()
+        speedLabel.text = "Speed of an unlaiden Falcon: \(1.0/(1.01-speedSlider.value))"
+    }
+    @IBAction func stepperChange(sender: AnyObject) {
+        speedSlider.value = Float(speedStepper.value)
+        changeDuration()
+        speedLabel.text = "Speed of an unlaiden Falcon: \(1.0/(1.01-speedSlider.value))"
+    }
+    
+    func changeDuration(){
+        falcon1.animationDuration = Double(2.0 - speedSlider.value)
+        falcon1.startAnimating()
+        falcon2.image = UIImage.animatedImageWithImages(falconImageList, duration: Double(2.0-speedSlider.value))
+    }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
