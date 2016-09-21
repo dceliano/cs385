4e316a567aa6f9fda51d426275f3d8cac8fb6e36
T6A User <t6a@mobiles-imac-28.local>
2016-09-21 13:38:40 -0600

    Downloaded the lab 14 template.

---
+++ LABs/Lab14_Gestures-2/Lab14_Gestures_Solution/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Lab14_Gestures_Solution
+//
+//  Created by Troy Weingart on 9/20/16.
+//  Copyright © 2016 Troy Weingart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
