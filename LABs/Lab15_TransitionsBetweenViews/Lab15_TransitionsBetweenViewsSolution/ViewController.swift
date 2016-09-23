b61d84884490f92dc492c803870eae4609b73e73
T6A User <t6a@mobiles-imac-28.local>
2016-09-23 14:21:37 -0600

    "Checked" all the files for lab 15.

---
+++ LABs/Lab15_TransitionsBetweenViews/Lab15_TransitionsBetweenViewsSolution/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Lab15_TransitionsBetweenViewsSolution
+//
+//  Created by Troy Weingart on 9/23/16.
+//  Copyright © 2016 Troy Weingart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, PresentedVCDelegate {
+
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBAction func doUnwind(seg: UIStoryboardSegue!){
+        
+    }
+    
+    func acceptMessage(mesg: String){
+        statusLabel.text = mesg
+    }
+    
+    @IBAction func doPresent(sender: AnyObject) {
+        let pvc = PresentedViewController()
+        pvc.message = "from root"
+        pvc.modalTransitionStyle = .FlipHorizontal //what the cool kids do'
+        pvc.delegate = self
+        self.presentViewController(pvc, animated: true, completion: nil)
+        
+    }
+    @IBAction func doSegue(sender: AnyObject) {
+        self.performSegueWithIdentifier("mySegue", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
