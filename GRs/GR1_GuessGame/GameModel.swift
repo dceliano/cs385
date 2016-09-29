e5416771a88def0ad554c012b3188f8c1a7740cf
T6A User <t6a@mobiles-imac-28.local>
2016-09-29 13:29:30 -0600

    Blank GR.

---
+++ GRs/GR1_GuessGame/GameModel.swift
@@ -0,0 +1,25 @@
+//
+//  GameModel.swift
+//  GR1_GuessGame
+//
+//  Created by Troy Weingart on 9/29/16.
+//  Copyright © 2016 Troy Weingart. All rights reserved.
+//
+
+import Foundation
+
+class GameModel {
+    
+    // magicNumber delcaration goes up here
+    
+    enum result {
+        
+    }
+    
+    func tryAttempt(guess guess: Int) -> result {
+        
+    }
+    
+    // any other functions you may need go below here
+    
+}
